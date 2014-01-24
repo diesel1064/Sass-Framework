d52792651e2751e471967d4e2ee45ceb67b0ca06
Dave Evans <info@daveevans.me>
2014-01-24 18:26:04 -0500

    Added Framework File Structure

---
+++ css/_reset.scss
@@ -0,0 +1,5 @@
+/*!
+ * Framework v1.0.0 (https://github.com/diesel1064/Sass-Framework)
+ * Copyright 2014 Dave Evans Design
+ * Licensed under http://www.apache.org/licenses/LICENSE-2.0
+ */
---
+++ js/custom.js
@@ -0,0 +1,7 @@
+/*!
+ * Framework v1.0.0 (https://github.com/diesel1064/Sass-Framework)
+ * Copyright 2014 Dave Evans Design
+ * Licensed under http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+ // USE THIS FILE FOR CUSTOM JAVASCRIPT OR JQUERY
