066050850d19f5f15f911bc63023b50aab76b46d
Dave Evans <devans@allegroconsultants.com>
2014-01-30 15:01:28 -0500

    Corrected responsive features of navigation

--- js/framework.js
+++ js/framework.js
@@ -7,22 +7,89 @@
 
  	// NAVIGATION SCRIPT
  	$nav = $('.main-nav');
- 	$nav.find($('.parent')).hover(function(){
- 		$(this).find('ul:first').stop().slideToggle(200);
- 	});
- 	$('.menu-button').click(function(){
- 		$(this).toggleClass('open');
- 		$nav.toggleClass('mobile');
- 		$nav.stop().slideToggle(200);
- 	});
+ 	$parent = $('.parent');
+ 	$carrot = $('.carrot-wrap');
+ 	$child = $('.child');
+ 	$window_size = $(window).width();
 
+ 	if($window_size >= 768) {
+	 	$nav.addClass('lg').removeClass('sm').css('display','block').css('height','40px');
+		$parent.removeClass('closed').removeClass('open').children('ul').slideUp();
+		$('.menu-button').removeClass('open');
+		$('nav').removeClass('overflow');
+	}
+	$(window).resize(function(){
+		$window_size = $(window).width();
+		if($window_size >= 768) {
+		 	$nav.addClass('lg').removeClass('sm').css('display','block').css('height','40px');
+			$parent.removeClass('closed').removeClass('open').children('ul').slideUp();
+			$carrot.removeClass('hide');
+			$('.menu-button').removeClass('open');
+			$('nav').removeClass('overflow');
+		}
+	});
+	if($window_size < 768) {
+		$nav.addClass('sm').removeClass('lg').css('display','none').css('height','auto');
+		$('nav').addClass('overflow');
+		$parent.addClass('closed');
+		$('.menu-button').click(function(){
+	 		if($(this).hasClass('open')) {
+	 			$(this).removeClass('open');
+	 			$nav.stop().slideToggle(200);
+	 			$parent.removeClass('open').addClass('closed').children('ul').slideUp();
+	 			$carrot.removeClass('hide');
+	 		} else {
+	 			$(this).addClass('open');
+	 			$nav.stop().slideToggle(200);
+	 		}
+	 	});
+		$carrot.click(function(){
+			if($(this).parent('li').hasClass('closed')) {
+				$(this).addClass('hide');
+				$(this).parent('li')
+					.removeClass('closed')
+						.addClass('open')
+							.children('ul')
+								.slideDown(200);
+			}
+		});
+	}
+	$(window).resize(function(){
+	 	$window_size = $(window).width();
+ 		if($window_size < 768) {
+			$nav.addClass('sm').removeClass('lg').css('display','none').css('height','40px');
+			$parent.addClass('closed');
+			$('nav').addClass('overflow');
+			$('.menu-button').click(function(){
+		 		if($(this).hasClass('open')) {
+		 			$(this).removeClass('open');
+		 			$nav.stop().slideToggle(200);
+		 			$parent.removeClass('open').addClass('closed').children('ul').slideUp();
+		 			$carrot.removeClass('hide');
+		 		} else {
+		 			$(this).addClass('open');
+		 			$nav.stop().slideToggle(200);
+		 		}
+		 	});
+			$carrot.click(function(){
+				if($(this).parent('li').hasClass('closed')) {
+					$(this).addClass('hide');
+					$(this).parent('li')
+						.removeClass('closed')
+							.addClass('open')
+								.children('ul')
+									.slideDown(200);
+				}
+			});
+		}
+	});
+ 	
+	// NAV WIDTH
 	$top_nav_items = $nav.find('>li');
 	$count = $top_nav_items.length;
 	$nav_width = 100 / $count;
 	$nav_percent = $nav_width + "%";
-
 	$nav.find('>li').css('width',$nav_percent);
-
 	$nav.find('>li').click(function(){
 	     window.location=$(this).find("a").attr("href"); 
 	     return false;
